34d5e3262faa9df8c7c34c1460442820f5c97260
tinhtrinh <chumeoconxauxa@gmail.com>
2021-11-29 18:22:44 +0700

    comment code lỗi

--- packages/web-server/src/services/PhieuThuTien.js
+++ packages/web-server/src/services/PhieuThuTien.js
@@ -1,4 +1,4 @@
-const { PhieuThuTien, Xe, DoanhSo, ChiTietDoanhSo } = require('../models');
+const { PhieuThuTien, Xe, DoanhSo, ChiTietDoanhSo, PhieuTiepNhan } = require('../models');
 
 /* `````````````````````````````````` */
 // Put your custom services code below this line
@@ -64,3 +64,37 @@ exports.findOne = (id) => {
     return PhieuThuTien.findOne({ _id: id }).populate('MaPTT');
 }
 /* `````````````````````````````````` */
+
+// let { maXe } = await PhieuTiepNhan.findOne({ _id: maPTN }, { maXe: 1 });
+// let { maHieuXe } = await Xe.findOne({ _id: maXe }, { maHieuXe: 1 });
+// let date = new Date(ngaySC);
+// let ds = await DoanhSo.aggregate([{$project: { month: {$month: '$ThoiDiemDS'}, year: { $year: '$ThoiDiemDS'}, tongDS: 1}}, 
+//     {$match: { month: date.getMonth() + 1, year: date.getFullYear()}}]);
+
+//     if(!ds[0]){
+//         let newDS = await new DoanhSo({ThoiDiemDS: ngaySC, tongDS: 0});
+//         await newDS.save();
+//         let { _id } = await DoanhSo.findOne({ ThoiDiemDS: ngaySC }, { _id: 1 })
+//         let newCtds = await new ChiTietDoanhSo({
+//             maHieuXe: maHieuXe,
+//             soLuongSua: 1,
+//             tiLe: 0,
+//             tongTien: 0,
+//             maDoanhSo: _id
+//         });
+//         await newCtds.save();
+//     } else {
+//         let ctds = await ChiTietDoanhSo.findOne({ maDoanhSo: ds[0]._id, maHieuXe: maHieuXe });
+//         if(!ctds) {
+//             let newCtds = await new ChiTietDoanhSo({
+//                 maHieuXe: maHieuXe,
+//                 soLuongSua: 1,
+//                 tiLe: 0.5,
+//                 tongTien: 0,
+//                 maDoanhSo: ds[0]._id
+//             });
+//             await newCtds.save();
+//         } else {
+//             await ChiTietDoanhSo.updateOne({ maDoanhSo: ds[0]._id }, { soLuongSua: ctds.soLuongSua + 1 });
+//         }      
+//     }
